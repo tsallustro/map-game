bd445a512a5b3559b9c374fa65574ee3070a511c
Tony S <tsallustro@outlook.com>
2026-01-14 18:43:34 -0500

    Added hill terrain

--- scripts/Enums.gd
+++ scripts/Enums.gd
@@ -2,4 +2,4 @@ class_name Enums
 
 enum ViewMode { OWNER = 0, PROVINCE = 1, TERRAIN =2}
 
-enum TerrainType {FOREST, PLAIN, MOUNTAIN, DESERT}
+enum TerrainType {FOREST, PLAIN, MOUNTAIN, DESERT, HILL}
